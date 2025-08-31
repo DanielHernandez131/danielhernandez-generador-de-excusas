933f93ad96dcfd110dd28549b07086027adcd759
Daniel Hernández <d.hernandezt.96@gmail.com>
2025-08-31 09:37:17 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  // Función para elegir un elemento aleatorio
+  function randomChoice(array) {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+  }
+
+  // Función para generar la excusa
+  function generateExcuse() {
+    return randomChoice(who) + " " + randomChoice(action) + " " + randomChoice(what) + " " + randomChoice(when) + ".";
+  }
+
+  const excuseElement = document.getElementById("excuse");
+  if (excuseElement) {
+    excuseElement.textContent = generateExcuse();
+  }
 };
